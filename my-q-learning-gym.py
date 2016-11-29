7a1b374ee97d3edfb42a3ec4f24f799093a28a5c
williamd4112 <williamd4112@hotmail.com>
2016-11-30 01:36:50 +0800

    Solved CartPole-V0

--- my-q-learning-gym.py
+++ my-q-learning-gym.py
@@ -6,7 +6,7 @@ import random
 import pandas as pd
 
 # Used for discretelize
-NUM_BINS = 10
+NUM_BINS = 4
 STATE_BINS = []
 
 def discretelize(s):
@@ -26,36 +26,32 @@ if __name__ == '__main__':
     env = gym.make('CartPole-v0')
 
     env.monitor.start('/tmp/cartpole-experiment-1', force=True)
+    np.random.seed(0)
 
-    # Step 0. Setup constants
-    EPISLON = 0.25
+    EPISLON = 0.5
     EPISLON_DECAY_RATE = 0.99
-    ALPHA = 0.25
-    ALPHA_DECAY_RATE = 0.99
-    GAMMA = 1.0
+    ALPHA = 0.05
+    GAMMA = 0.99
     NUM_STATES = 10 ** (env.observation_space.shape[0])
     NUM_ACTIONS = (env.action_space.n)
-    MAX_EPISODES = 300
-    MAX_TIME_STEPS = 1000
+    MAX_EPISODES = 1000
+    MAX_TIME_STEPS = 200
 
-    # Step 1. preparaed discretelization params
-    lows = env.observation_space.low
-    highs = env.observation_space.high
+    # Trick: shrink the state space
+    lows = np.array([-2.4, -3.0, -0.5, -2.0])
+    highs = np.array([2.4, 3.0, 0.5, 2.0])
     bounds = np.hstack((np.hsplit(lows, len(lows)), np.hsplit(highs, len(highs))))
     for bound in bounds:
         (out, bins) = pd.qcut(bound, NUM_BINS, retbins=True)
         STATE_BINS.append(bins[1:-1])
 
-    Q = np.random.uniform(low=0, high=1, size=(NUM_STATES, NUM_ACTIONS))
+    Q = np.zeros((NUM_STATES, NUM_ACTIONS))
     A = np.matlib.repmat(np.arange(NUM_ACTIONS), NUM_STATES, 1)
 
     for i_episode in xrange(MAX_EPISODES):
         state = env.reset()
         state = discretelize(state)
         for t in xrange(MAX_TIME_STEPS):
-            env.render()
-
-            # Step 4. Under s, take action from A(s)
             action = epislon_policy(Q, A, state, EPISLON)
 
             next_state, reward, done, info = env.step(action)
@@ -63,13 +59,15 @@ if __name__ == '__main__':
 
             if done:
                 print "Episode {} finished after {} timesteps ( {}, {} )".format(i_episode, t+1, ALPHA, EPISLON)
-                break
+                if t < MAX_TIME_STEPS - 1:
+                    reward = -MAX_TIME_STEPS
 
             # Step 5. Update Q(s, a)
             Q[state][action] = Q[state][action] + ALPHA * (reward + GAMMA * np.max(Q[next_state]) - Q[state][action])
 
-            EPISLON *= EPISLON_DECAY_RATE
-            ALPHA *= ALPHA_DECAY_RATE
             state = next_state
+            if done:
+                break
+        EPISLON *= EPISLON_DECAY_RATE
     env.monitor.close()
     gym.upload('/tmp/cartpole-experiment-1', api_key='sk_nRXxhE9sTIqcYJ1DOGDew')
